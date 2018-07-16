c75f414ab54abfd94623fb62c2716cd3810ee65f
mshczhanglidong <zhangyong87425712@qq.com>
2018-07-16 22:44:58 +0800

    更新了xxx

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!sdsdsd");
     }
+    ArrayList arrayList=new ArrayList();
+    ArrayList arrayList2=new ArrayList();
+    
 }
--- src/xxx.java
+++ src/xxx.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class xxx {
+    int a = 1;
+    ArrayList<String> ArrayList = new ArrayList<String>();
+
+    public void setA(int a) {
+        for (String o : ArrayList) {
+        }
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
